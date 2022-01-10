029017d5d8a109de21f275172d55ca15bec934de
Taeyeon <xodus05>
2022-01-10 13:14:52 +0900

    get 메소드 둘레 계산 메소드

---
+++ src/test/this2/Circle.java
@@ -0,0 +1,29 @@
+package test.this2;
+
+public class Circle {
+	private int r;
+	private double circum, area;
+	
+	public Circle(int r) {
+		this.r = r;
+		
+		
+	}
+
+	public int getR() {
+		return r;
+	}
+
+	public double getCircum() {
+		return circum;
+	}
+
+	public double getArea() {
+		return area;
+	}
+	
+// 반지름이 r인 원의 둘레계산하는 메소드
+	public void calcCircum() {
+		circum = 2 * Math.PI * r;
+	}
+}
