a20b206b61fc8ec23167835547f83a209dd0cac4
Taeyeon <xodus05>
2022-01-10 15:28:21 +0900

    RunTV

--- src/test/static1/TV.java
+++ src/test/static1/TV.java
@@ -1,7 +1,7 @@
 package test.static1;
 
 public class TV {
-	private String producer; //클래스 변수 공유영역에 단 1번만 할당됨
+	public static String producer; //클래스 변수 공유영역에 단 1번만 할당됨
 	private int size; // 인스턴스변수 객체 생성될 떄 마다 독립된 멤버 필드로 할당됨
 	private String color; // 인스턴스변수
 	
